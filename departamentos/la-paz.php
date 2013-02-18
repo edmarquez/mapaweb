5f3ad2bc7547fe911df27cebba07bfd91680a3c9
David Marquez <david.marquz@gmail.com>
2013-02-18 17:52:33 -0600

    Primero para mapa

---
+++ departamentos/la-paz.php
@@ -0,0 +1,46 @@
+<html>
+	<head>
+		<title></title>
+		<meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+        <meta name="description" content="Original Hover Effects with CSS3" />
+        <meta name="keywords" content="css3, transitions, thumbnail, animation, hover, effect, description, caption" />
+        <meta name="author" content="Alessio Atzeni for Codrops" /> 
+		<link rel="stylesheet" href="../css/css.css" />
+		
+		
+        <link rel="stylesheet" type="text/css" href="../css/demo.css" /> 
+        <link rel="stylesheet" type="text/css" href="../css/style_common.css" />
+        <link rel="stylesheet" type="text/css" href="../css/style5.css" />
+		
+		 <link rel="stylesheet" href="css/ico/css/fontello.css"><!--[if IE 7]>
+    <link rel="stylesheet" href="css/ico/css/fontello-ie7.css"><![endif]-->
+	</head>
+	<body>
+		<div id="barra">
+		</div>
+		<nav role="navigation">
+			<div class="inner group"> 
+				<ul>
+					<li><a href="#">Inicio</a></li>
+					<li><a href="#">Con&oacute;cenos</a></li>
+					<li><a href="#">Servicios</a></li>
+					<li><a href="#">Portafolio</a></li>
+					<li><a href="#">Blog</a></li>
+					<li><a href="#">Contacto</a></li>
+				</ul>
+			</div>
+		</nav>
+	<div id="wrapper">
+		<div id="section"> 
+			 
+		</div><!--end div Section-->
+		 <div class="clr"></div>  
+	</div><!--end Wrapper-->
+	
+<?php
+	include_once('../templates/bottom.php');
+?>
+	</body>
+</html>
---
+++ templates/bottom.php
@@ -0,0 +1,40 @@
+<!-- start footer -->         
+<div id="footer">
+	<!--  start footer-left -->
+	<div id="footer-left">
+	
+		<ul class="red"> 
+				<li>
+					<a href="https://twitter.com/David_Marq"><i class="icon-twitter-1"></i> </a>
+					<span class="tooltip"><span>Siguenos en Twitter</span></span>
+				</li>
+				<li>
+					<a href="https://twitter.com/David_Marq"><i class="icon-gplus-1"></i> </a>
+					<span class="tooltip"><span>Sumate a nosotros en Google-plus</span></span>
+				</li>
+				<li>
+					<a href="https://twitter.com/David_Marq"><i class="icon-facebook-1"></i> </a>
+					<span class="tooltip"><span>Se nuestro fan en Facebook</span></span>
+				</li>
+		</ul> 
+	<h2>MAKING</h2> &copy; algunos derechos reservados 2012-2013. <br><span>echolab: Desarrolladores</span> 
+	</div><!--  end footer-left --> 
+	<div id="footer-right">
+	
+		<form id="contact" method="POST" name="contact" autocomplete="off">
+			<h2>CONTACTANOS</h2>
+			<label>Correo Electr&oacute;nico</label><br />
+			<input type="email" class="in-contact" name="correo"></input><br /><br />
+			<label>Mensaje</label><br />
+			<textarea name="mensaje" id="text-mensaje" rows="5" cols="21"  maxlength="2000"></textarea><br /><br /> 
+			<input type="submit" class="sub-contact"></input>
+		</form>
+	</div><!--  end footer-right --> 
+	
+	<div class="clear"> </div>
+</div>
+<!-- end footer -->
+ 
+</body>
+</html>
+ 
