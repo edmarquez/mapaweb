c4410253846e70f599e98d929a17cbb0a0a295d6
David Marquez <david.marquz@gmail.com>
2013-02-22 07:58:35 -0600

    consultas

---
+++ inicio.php
@@ -0,0 +1,30 @@
+ <html>
+     <head>
+	       <title>esqueleto</title>
+		   <link rel="stylesheet" type="text/css"href="css.css"/>
+     </head>
+	 <body>
+	       <div id="contenedor-principal">
+		           <div id="encabezado">
+				            <p>this es the header</p>
+                   </div>   
+	               <div id="menu-navegacion">
+		                    <p>this the menu</p>
+					</div>   
+	               <div id="columna-izquierda">
+		                    <p>this the text leftcolumn</p>
+					</div>
+					<div id="contenedor">
+		                    <p>this the text content</p>
+					</div>
+					<div id="columna-derecha">
+		                    <p>this the text rightcolumn</p>
+					</div>
+					<div id="pie-pagina">
+					        <p>this is the footer</p>
+					</div>		
+					<a href="miweb.html">mi web</a>
+					
+			</div>
+	 </body>
+ </html>
